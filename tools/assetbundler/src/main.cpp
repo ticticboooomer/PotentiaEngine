275a396934842d3fe6cad650f60cd3f6fa6527be
ticticboooomer <kyle.benjamin.cox@outlook.com>
2022-10-17 14:44:59 +0100

    started importer + asset bundler

--- tools/assetbundler/src/main.cpp
+++ tools/assetbundler/src/main.cpp
@@ -3,6 +3,7 @@
 int main(int argc, char** argv) {
     CLI::App app{"Asset bundler for Potentia Engine"};
 
+    
 
     CLI11_PARSE(app, argc, argv);
 }
--- tools/importer/src/main.cpp
+++ tools/importer/src/main.cpp
@@ -3,6 +3,7 @@
 int main(int argc, char** argv) {
     CLI::App app{"Mesh file importer for Potentia Engine"};
 
+    CLI::App* sub = app.add_subcommand("mesh", "Imports a 3D mesh file into the engine format(s).");
 
     CLI11_PARSE(app, argc, argv);
 }
